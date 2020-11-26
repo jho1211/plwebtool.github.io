9b6ab321e7e7a623ac31848330e29be7d7fcf3c5
Lab Member 3 <jeffreyho1211@gmail.com>
2020-11-26 17:35:31 -0500

    V0.18
    Completed functionality of tRF sequence finder, but need to fix still.

--- tools/js/tool1.js
+++ tools/js/tool1.js
@@ -1,20 +1,30 @@
-function runTool(fasta, numTRFs, genome){
+async function getData(url){
+
+  const response = await fetch(url);
+
+  return response.text();
+}
+
+async function runTool(fasta, numTRFs, genome, outputid){
   var input = document.getElementById(fasta).value;
   var refFileName = document.getElementById(genome).value;
 
   try{
     var fileName = document.getElementById(genome).value;
-    fetch('https://plwebtool.github.io/tools/db/ecoli-tRNA-db.txt')
-      .then(response => response.text())
-      .then(text => console.log(text))
+    var ref;
+
+    ref = await getData('https://plwebtool.github.io/tools/db/' + fileName + '.txt');
   }
   catch(err){
-    console.log(err);
+    alert('Species not implemented yet.');
   }
 
-  console.log(refFileName);
+  var output = findTRFs(input, ref)
+
+  document.getElementById(outputid).value = output;
 }
 
+/*
 function binarySearch(seq, ref){
   var first = 0;
   var last = ref.length - 1;
@@ -40,7 +50,104 @@ function binarySearch(seq, ref){
 
   return found
 }
+*/
 
-function generateSeqs(fasta, ref){
+function oddArray(arr){
+  // Returns the odd numbered index elements
+  var newArr = [];
 
+  for (i = 0; i < arr.length; i++){
+
+    if ((i % 2) == 1){
+      newArr.push(arr[i]);
+    }
+  }
+
+  return newArr
+}
+
+function evenArray(arr){
+  // Returns the even numbered index elements
+  var newArr = [];
+
+  for (i = 0; i < arr.length; i++){
+
+    if ((i % 2) == 0){
+      newArr.push(arr[i]);
+    }
+  }
+
+  return newArr
+}
+
+function generateSubsequences(seq){
+  var seq_ignore_first = seq.slice(1);
+  var seq_ignore_first_two = seq.slice(2);
+  var seq_ignore_last_two = seq.slice(0, seq.length - 2);
+  var seq_ignore_last = seq.slice(0, seq.length - 1);
+  var seq_ignore_first_last = seq.slice(1, seq.length - 1);
+
+  return [seq, seq_ignore_first, seq_ignore_first_two, seq_ignore_last_two, seq_ignore_last, seq_ignore_first_last];
+}
+
+function trfName(desc){
+
+  // Returns the name of the tRF the sequence corresponds to
+  if (desc[11] == '-'){
+      return desc.slice(3, 11)
+      }
+    else{
+      return desc.slice(3, 12);
+    }
+}
+
+function searchRef(seq, ref){
+  /* Search for the seq in the reference text
+     Return the name of the tRF it corresponds to if found */
+
+  var refArray = ref.split('\n');
+  var refDescs = evenArray(refArray);
+  var refSeqs = oddArray(refArray);
+
+  for (i = 0; i < refSeqs.length; i++){
+    if (refSeqs[i].includes(seq)){
+      return trfName(refDescs[i]);
+    }
+  }
+
+  return false; // return false if nothing is found
+}
+
+function isTRF(seq, ref){
+  var subseqs = generateSubsequences(seq)
+
+  for (i = 0; i < subseqs.length; i++){
+    var result = searchRef(subseqs[i], ref);
+
+    if (typeof(result) == "string"){
+      return result + ', sequence matched: ' + subseqs[i];
+    }
+  }
+
+  return false;
+}
+
+function findTRFs(fasta, ref){
+  var fastaArray = fasta.split('\n');
+
+  var descs = evenArray(fastaArray);
+  var seqs = oddArray(fastaArray);
+
+  var newFastaArray = []
+
+  for (i = 0; i < seqs.length; i++){
+    var result = isTRF(seqs[i], ref)
+
+    if (typeof(result) == "string"){
+      newFastaArray.push(descs[i] + ' (' + result + ' )');
+      newFastaArray.push(seqs[i]);
+    }
+  }
+
+  return newFastaArray.join('\n');
 }
