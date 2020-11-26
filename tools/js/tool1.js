1e50649c88e7b387173700aa4944d93be92ae04a
Lab Member 3 <jeffreyho1211@gmail.com>
2020-11-26 15:03:56 -0500

    V0.18
    Began implementation of tRF sequence finder tool.

--- tools/js/tool1.js
+++ tools/js/tool1.js
@@ -1,3 +1,45 @@
-function runTool(){
-  alert("Hello World");
+function runTool(fasta, numTRFs, genome){
+  var input = document.getElementById(fasta).value;
+  var refFileName = document.getElementById(genome).value;
+
+  try{
+    var fileName = document.getElementById(genome).value;
+    var fileToRead = fetch('db/' + fileName + '.txt')
+    console.log(fileToRead);
+  }
+  catch(err){
+    console.log(err);
+  }
+
+  console.log(refFileName);
+}
+
+function binarySearch(seq, ref){
+  var first = 0;
+  var last = ref.length - 1;
+  var i = -1;
+  var found = false;
+
+  while ((i == -1) && (first <= last)){
+    var mid = Math.floor((first + last) / 2);
+
+    if (ref[mid] == seq){
+      i = mid;
+      found = true;
+    }
+
+    else if (seq < ref[mid]){
+      last = mid - 1
+    }
+
+    else{
+      first = mid + 1
+    }
+  }
+
+  return found
+}
+
+function generateSeqs(fasta, ref){
+
 }
